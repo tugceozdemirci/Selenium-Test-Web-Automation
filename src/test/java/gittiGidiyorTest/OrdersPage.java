fd8c4677ba98507f02e1aecf1e69c4be187b697e
dmirci2@gmail.com <dmirci2@gmail.com>
2021-05-22 20:05:52 +0300

    Commit

--- src/test/java/gittiGidiyorTest/OrdersPage.java
+++ src/test/java/gittiGidiyorTest/OrdersPage.java
@@ -1,24 +1,20 @@
 package gittiGidiyorTest;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import java.util.List;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class OrdersPage {
-
     public OrdersPage(WebDriver driver) {
 
     }
 
-    public void AddProduct() {
 
-    }
 
-    public void DeleteProduct() {
 
-    }
+
+
 
 
 }
